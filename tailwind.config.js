377e64e2bd75153cfdd51094146ef22e85ac63e1
hilanoam <hilanoam123@gmail.com>
2026-02-09 10:44:02 +0200

    סיום פרונט

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./*.js"],
+  content: ["./*.html", "./**/*.html", "./*.js", "./**/*.js"],
   theme: {
     extend: {},
   },
